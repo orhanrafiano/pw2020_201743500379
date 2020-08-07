cacdd2774cd3c4d3aad8c6559116325d99b611be
orhanrafiano <69307943+orhanrafiano@users.noreply.github.com>
2020-08-07 16:06:14 +0700

    Add files via upload

---
+++ kuliah/pertemuan10/detail.php
@@ -0,0 +1,32 @@
+<?php
+require 'functions.php';
+
+// Ambil Id dari URL
+$id = $_GET['id'];
+
+//Query mahasiswa berdasarkan ID
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>"></li>
+    <li>NPM : <?= $m['npm']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
+    <li><a href="">Ubah</a> | <a href="">Hapus</a></li>
+    <li><a href="latihan3.php">Kembali</a></li>
+  </ul>
+</body>
+
+</html>
